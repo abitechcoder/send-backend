7e8dc23d82454a5db9d5d527f55c2b1807c54197
Adeosun Abiola <abiolaolalekan39@gmail.com>
2024-10-31 10:36:02 +0100

    Added another allowed origin

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'https://sendsierraleone.com'],
+  origin: [
+    'http://localhost:5173',
+    'https://sendsierraleone.com',
+    'https://www.sendsierraleone.com/',
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
